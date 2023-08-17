731e294b0c2d29841dd7e74b4e54f52961b81546
djouani.taqiyeddine <djouani.taqiyeddine@gmail.com>
2023-08-17 18:16:10 +0200

    fixing docker build command

--- script.groovy
+++ script.groovy
@@ -1,12 +1,12 @@
 def buildDockerImage() {
     echo "Building the docker image...."
     withCredentials([usernamePassword(credentialsId:'dockr-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){
-        sh "docker build -t taqiyeddinedj/ci_cd_pipline:web-app:1.0 ."
+        sh "docker build -t taqiyeddinedj/ci_cd_pipline:webapp:1.0 ."
     }
 }
 def pushtoHub() {
     sh "echo $PASS | docker login -u $USER --passwd-stdin"
-    sh "docker push taqiyeddinedj/ci_cd_pipline:web-app:1.0"
+    sh "docker push taqiyeddinedj/ci_cd_pipline:webapp:1.0"
 }
 
 def deploytok8s() {
