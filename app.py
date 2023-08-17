2684f41666494ec258963538751789caad8f6643
djouani.taqiyeddine <djouani.taqiyeddine@gmail.com>
2023-08-17 23:19:11 +0200

    fixing code

--- app.py
+++ app.py
@@ -4,11 +4,9 @@ import socket
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return """
-    <h1>Hello I am taqiyeddine</h1>
-    <p>This app is running on node: {}</p> 
-    """.format(socket.gethostname())
+def get_hostname():
+    hostname = socket.gethostname()
+    return f"The hostname of the server is: {hostname}"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
