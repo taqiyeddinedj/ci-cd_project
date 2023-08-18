af98b5d7c0fde4d8f5290792121eb936cb654b2e
djouani.taqiyeddine <djouani.taqiyeddine@gmail.com>
2023-08-18 10:41:06 +0200

    adding htl to app.py

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ import subprocess
 app = Flask(__name__)
 
 @app.route('/')
+def index():
+    return "<h1>Hello World!</h1>" 
+
+@app.route('/hostname')
 def print_hostname():
     hostname = subprocess.run(['hostname'], capture_output=True)
-    return hostname.stdout.decode()
+    return "<pre>" + hostname.stdout.decode() + "</pre>"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
